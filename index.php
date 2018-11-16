98eed087309560b5c9a0b8ff80401e16865f2413
MovieHoarder <45087950+MovieHoarder@users.noreply.github.com>
2018-11-15 21:46:07 -0700

    index

---
+++ index.php
@@ -0,0 +1,109 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Movies</title>
+  <meta name="description">
+  <meta name="author" content="SitePoint">
+  <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+
+<body>
+    <h1 style="color:white; text-align:center">Watch Your Favorite Movies</h1>
+    <ul>
+  <li><a class="active" href="#home">Home</a></li>
+  <li><a href="Request.php">Request</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <li><a href="#about">About</a></li>
+</ul>
+    <br>
+ <table>
+     <tr>
+     <td>
+    <a href="http://192.168.0.4/Movies/Movie_Files/Avengers%20Infinity%20war.mp4">
+     <img src="http://192.168.0.4/Movies/images/Avengers.jpg" style="width:50%">
+     </a>
+         <h3><a href="http://192.168.0.4/Movies//Movie_Files/Avengers%20Infinity%20war.mp4" style="text-decoration:none" class="links">Avengers Infinity War</a></h3>
+     </td>
+         
+    <td>
+    <a href="http://192.168.0.4/Movies/Movie_Files/Sorry%20to%20bother%20you.mp4">
+     <img src="http://192.168.0.4/Movies/images/sorry_to_bother_you.jpg" style="width:50%">
+     </a>
+         <h3><a href="http://192.168.0.4/Movies/Movie_Files/Sorry%20to%20bother%20you.mp4" style="text-decoration:none" class="links">Sorry to Bother You</a></h3>
+     </td>
+         
+     <td>
+    <a href="http://192.168.0.4/Movies/Movie_Files/The%20Incredibles%202.mp4">
+     <img src="http://192.168.0.4/Movies/images/incredibles-2-l_cover.jpg" style="width:50%">
+     </a>
+         <h3><a href="http://192.168.0.4/Movies/Movie_Files/The%20Incredibles%202.mp4" style="text-decoration:none" class="links">The Incredibles 2</a></h3>
+     </td>
+     
+     <td>
+    <a href="http://192.168.0.4/Movies/Movie_Files/BlacKkKlansman.mp4">
+     <img src="http://192.168.0.4/Movies/images/BlacKkKlansman-Poster.jpg" style="width:50%">
+     </a>
+         <h3><a href="http://192.168.0.4/Movies/Movie_Files/BlacKkKlansman.mp4" style="text-decoration:none" class="links">BlacKkKlansman</a></h3>
+     </td>
+     </tr>
+         
+         
+      <tr>   
+    <td>
+    <a href="http://192.168.0.4/Movies/Movie_Files/Solis.mp4">
+     <img src="http://192.168.0.4/Movies/images/thESPAWQI3.jpg" style="width:50%">
+     </a>
+         <h3><a href="http://192.168.0.4/Movies/Movie_Files/Solis.mp4" style="text-decoration:none" class="links">Solis</a></h3>
+     </td>
+         
+     <td>
+    <a href="http://192.168.0.4/Movies/Movie_Files/The%20Predator.mp4">
+     <img src="http://192.168.0.4/Movies/images/The-Predator-Poster.jpg" style="width:50%">
+     </a>
+         <h3><a href="http://192.168.0.4/Movies/Movie_Files/The%20Predator.mp4" style="text-decoration:none" class="links">The Predator</a></h3>
+     </td>
+         
+          <td>
+    <a href="http://192.168.0.4/Movies/Movie_Files/Ready%20player%20one.mp4">
+     <img src="http://192.168.0.4/Movies/images/Ready-Player-One-1.jpg" style="width:50%">
+     </a>
+         <h3><a href="http://192.168.0.4/Movies/Movie_Files/Ready%20player%20one.mp4" style="text-decoration:none" class="links">Ready Player One</a></h3>
+     </td>
+          
+        <td>
+    <a href="http://192.168.0.4/Movies/Movie_Files/The.Darkest.Minds.mp4">
+     <img src="http://192.168.0.4/Movies/images/the-darkest-minds-movie-poster.jpg" style="width:50%">
+     </a>
+         <h3><a href="http://192.168.0.4/Movies/Movie_Files/The.Darkest.Minds.mp4" style="text-decoration:none" class="links">The Darkest Minds</a></h3>
+     </td>
+         
+     </tr>
+     
+     
+     
+     <tr>
+     
+      <td>
+    <a href="http://192.168.0.4/Movies/Movie_Files/Ant-man%20And%20The%20Wasp.mp4">
+     <img src="http://192.168.0.4/Movies/images/antman%20and%20the%20wasp.jpg" style="width:50%">
+     </a>
+         <h3><a href="http://192.168.0.4/Movies/Movie_Files/Ant-man%20And%20The%20Wasp.mp4" style="text-decoration:none" class="links">Antman and The Wasp</a></h3>
+     </td>
+         
+         
+         <td style="border-right:1px solid gray">
+    <a href="http://192.168.0.4/Movies/Movie_Files/Jumanji.mp4">
+     <img src="http://192.168.0.4/Movies/images/Jumanji.jpg" style="width:50%">
+     </a>
+         <h3><a href="http://192.168.0.4/Movies/Movie_Files/Jumanji.mp4" style="text-decoration:none" class="links">Jumanji</a></h3>
+     </td>
+     
+     </tr>
+    
+</table>
+    
+</body>
+</html>
